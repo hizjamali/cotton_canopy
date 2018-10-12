67221cacaac66fe044e4751459af168dcd858cb4
Hiz Jamali <hiz.jamali@csiro.au>
2018-10-12 15:41:26 +1100

    script completed and results comiled in RMarkdown

--- scripts/canopy_temp_prediction.R
+++ scripts/canopy_temp_prediction.R
@@ -17,8 +17,8 @@ View(agro)
 agro$lint_yield_kg_per_ha <- factor(agro$lint_yield_kg_per_ha)
 class(agro$lint_yield_kg_per_ha)
 
-# load weather data
-weather_raw <- read.csv("data/ACRI_2001_2017.csv") %>% 
+# upload weather data
+weather_raw <- read.csv("C:/Users/jam19e/dataschool/cotton_canopy/data/ACRI_2001_2017.csv") %>% 
   filter(between(Year,2015, 2016))
 
 str(weather_raw)
@@ -27,6 +27,9 @@ head(weather_raw)
 colnames(weather_raw) <- c("datetime", "Year", "Day","Hour",
                            "maxAT","minAT", "maxST","minST","maxRH","minRH", "WS", "WD","MaxWS", "solar_rad", "rain","tot_rain")
 
+#Plot weather data
+
+ggplot(weather_raw , aes(datetime, maxAT)) + geom_smooth()
 
 #weather <- NULL
 #system.time(
@@ -45,7 +48,7 @@ weather <- weather_raw  %>%
          meanRH = (maxRH + minRH)/2) %>% 
   select(Year, Day, Hour, meanAT, meanRH, WS, solar_rad, rain)
 
-tz(weather_raw$datetime)
+tz(weather$datetime)
 head(weather)
 
 #Plot weather data
@@ -59,8 +62,8 @@ ggplot(weather , aes(datetime, meanAT)) + geom_smooth()
 #setwd(file.path("C:/Users/jam19e/dataschool/cotton_canopy/data/canopy/sensors/")) or use actual file path
 #Create a file that lists filenames
 
-file_names <- list.files(path = "C:/Users/jam19e/dataschool/cotton_canopy/data/canopy/sensors/", 
-                         full.names = T, pattern = "*.csv") %>% 
+file_names <- list.files(path = "C:/Users/jam19e/dataschool/cotton_canopy/data/canopy", 
+                         full.names = T, pattern = "*.csv") 
 
 # Create a new dataframe that combines all sensor files and adds a column with sensor ID
 sensor_all <- data_frame(filename = file_names) %>%
@@ -93,34 +96,36 @@ canopy_temp <- sensor_extract %>%
          Year = year(datetime),
          Day = yday(datetime),
          Hour = hour(datetime)) %>% 
-  select(ID, datetime, Year, Day, Hour, canopytemp) %>% 
+  mutate(Treatment=ifelse(ID =="2015_ACRI_LW_3"|ID =="2015_ACRI_LW_5", "Irrigated","Stressed"))%>% 
+  select(ID, datetime, Treatment, Year, Day, Hour, canopytemp) %>% 
   ungroup()
 
 
 
 #Combine CT and weather data
-canopy_weather <- full_join(canopy_temp,weather)
-
+canopy_weather <- full_join(canopy_temp,weather) %>% 
+  filter(!is.na(ID))
+  
 # Visulaize weather and canopy temperature data
 
-ggplot(data = canopy_weather,
-       aes(x=meanAT, y= canopytemp))+
-  geom_line(size = 1)+
-  facet_wrap(~ID) %>% 
-filter(between(Day,1, 60))
+ggplot(data=subset(canopy_weather, Day%in% c(1)),
+       aes(x=datetime, y= meanAT))+
+  geom_line()+
+  facet_wrap(~ID)
+
 
 
 # Model data
-lm_bubbles <- lm(biomass_g_per_pot~treatment + block + position, data=bubbles) #
-anova(lm_bubbles)
-summary(lm_bubbles)
+lm_CT <- lm(canopytemp~meanAT + ID, data=canopy_weather) #
+anova(lm_CT)
+summary(lm_CT)
 
-lmer_bubbles <- lmer(biomass_g_per_pot~treatment+(1|block) + position, data=bubbles)
-anova(lmer_bubbles)
-summary(lmer_bubbles)
-plot(lmer_bubbles)
+lmer_CT <- lmer(sqrt(canopytemp)~meanAT+ meanRH + solar_rad + (1|ID) , canopy_weather)
+anova(lmer_CT)
+summary(lmer_CT)
+plot(lmer_CT)
 
-emmeans(lmer_bubbles, pairwise~treatment|species)
+emmeans(lmer_CT, pairwise~canopytemp|ID)
 
 
 
